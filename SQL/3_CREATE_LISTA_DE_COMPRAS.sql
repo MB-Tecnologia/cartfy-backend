feedf30e958d3358c3caf8bcfebb79b3d2cd81de
Joao Augusto <joaootsugua@gmail.com>
2025-04-26 21:22:24 -0300

    Organiza scripts sql

--- SQL/3_CREATE_LISTA_DE_COMPRAS.sql
+++ SQL/3_CREATE_LISTA_DE_COMPRAS.sql
@@ -1,16 +1,3 @@
-CREATE DATABASE CARTFY;
-
-CREATE TABLE usuario (
-	id int NOT NULL AUTO_INCREMENT,
-	cpf_cnpj varchar(14),
-	nome varchar(50),
-    email varchar(320),
-    senha varchar(200),
-    dt_incluso datetime
-);
-
-
-
 CREATE TABLE lista_de_compras (
 	id_lista bigint NOT NULL AUTO_INCREMENT,
     id_usuario int NOT NULL ,
