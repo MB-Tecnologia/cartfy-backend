4561f83f0f3f58629a3e1f2e5707446d26071fa5
Joao Augusto <joaootsugua@gmail.com>
2025-05-05 19:36:23 -0300

    Add cache Caffeine

---
+++ src/main/java/com/cartfy/backend/cartfy_backend/config/CacheConfig.java
@@ -0,0 +1,24 @@
+package com.cartfy.backend.cartfy_backend.config;
+
+import com.github.benmanes.caffeine.cache.Caffeine;
+import org.springframework.cache.CacheManager;
+import org.springframework.cache.caffeine.CaffeineCacheManager;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.concurrent.TimeUnit;
+
+@Configuration
+public class CacheConfig {
+
+    @Bean
+    public CacheManager cacheManager() {    
+        CaffeineCacheManager manager = new CaffeineCacheManager("productGtinCache", "productTermCache");
+        manager.setCaffeine(
+            Caffeine.newBuilder()
+            .expireAfterWrite(12, TimeUnit.HOURS)
+            .maximumSize(10000)
+            );
+        return manager;
+    }
+}
